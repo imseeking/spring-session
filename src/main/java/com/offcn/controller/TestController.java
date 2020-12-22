6809d983977c36666c375f82d3fc25b6b32316a0
imseeking <1150523831@qq.com>
2020-12-22 22:00:25 +0800

    12月22日 测试idea提交项目到github

---
+++ src/main/java/com/offcn/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("set")
+    public String setSession(HttpSession session){
+        session.setAttribute("msg","Hello Spring Session");
+        return "ok";
+    }
+
+    @GetMapping("get")
+    public String getSession(HttpSession session){
+        String msg = (String) session.getAttribute("msg");
+        return msg;
+    }
+
+}
